fb8d819a1b180abf66dda267d97f5b4093e8ee12
MijanurRahman40 <mijanurrahman31416@gmail.com>
2020-07-23 19:13:46 +0600

    changes

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
-// dbPassword = 'mongodb+srv://raju_31416:4ilovesherpur@outhpassport-qbffa.mongodb.net/test?retryWrites=true&w=majority';
-database = 'mongodb://127.0.0.1:27017/outh_passport';
+
+database = 'mongodb + srv://raju_31416:<123456789>@cluster0.qbffa.mongodb.net/<auth_passport>?retryWrites=true&w=majority';
+// database = 'mongodb://127.0.0.1:27017/outh_passport';
 
 module.exports = {
     mongoURI: database
