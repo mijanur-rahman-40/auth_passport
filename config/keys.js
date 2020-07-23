55f32389d843af331bcd192b2ec402e8167fa08d
MijanurRahman40 <mijanurrahman31416@gmail.com>
2020-07-23 20:13:34 +0600

    changed

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 
-database = 'mongodb + srv://raju_31416:4ilovesherpur@cluster0.qbffa.mongodb.net/auth_passport?retryWrites=true&w=majority';
-// database = 'mongodb://127.0.0.1:27017/outh_passport';
+// database = 'mongodb + srv://raju:4ilovesherpur@cluster0.qbffa.mongodb.net/auth_passport?retryWrites=true&w=majority';
+database = 'mongodb://127.0.0.1:27017/outh_passport';
 
 module.exports = {
     mongoURI: database
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,18 @@ const bcrypt = require('bcryptjs');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    service: 'Gmail',
+    auth: {
+        user: 'docopediaweb@gmail.com', // generated ethereal user
+        pass: 'R_r123456', // generated ethereal password
+    },
+});
 
 // User Model
 const User = require('../model/User');
@@ -76,6 +88,24 @@ router.post('/register', (request, response) => {
                         // Save User
                         newUser.save()
                             .then(user => {
+
+                               let mailOptions = {
+                                    from: 'docopediaweb@gmail.com',
+                                    to: email,
+                                    subject: "Please confirm your Gmail account by",
+                                    html: "Hello,<br> Please give the code value to verify your email.<br>"
+                                };
+                                // console.log(mailOptions);
+                                transporter.sendMail(mailOptions, function (error, response) {
+                                    if (error) {
+                                        console.log(error);
+                                        // res.end("error");
+                                    } else {
+                                        console.log("Message sent");
+                                        // res.end("sent");
+                                    }
+                                });
+
                                 request.flash('success_message', 'You are now registered and can log in..');
                                 response.redirect('/user/login');
                             })
